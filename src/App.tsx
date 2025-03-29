efb9a39eb9ab73a5406e0ace8f9ace96750d52ae
Sergii Vdovareize <servdo.gmail.com>
2025-03-29 12:30:54 +0200

    upd

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BalanceTracker from './components/BalanceTracker';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Balance Tracker</h1>
+      <BalanceTracker />
+    </div>
+  );
+}
+
+export default App; 
