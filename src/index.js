0f329244ac75ba76f80c2bfce82d30eb80dd0e0c
Sergii Vdovareize <servdo.gmail.com>
2025-04-27 09:38:02 +0300

    init senry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import * as Sentry from "@sentry/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://60b42e0c00dfcba2ec7b4da10d16d57f@o4508043966087168.ingest.de.sentry.io/4509223860174928",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
