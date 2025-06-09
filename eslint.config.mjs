a4d95c9c8afaabee1800998df1da3a16701e8a11
Sergii Vdovareize <servdo.gmail.com>
2025-06-09 09:49:32 +0300

    lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,17 @@ import pluginReact from "eslint-plugin-react";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.node } },
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: { ...globals.browser, process: "readonly" } } },
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/prop-types": "off",
+    },
+  },
 ]);
